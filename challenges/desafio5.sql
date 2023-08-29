fef14ae5684258cf9e49ebdfb21d9ac4304d52d7
Lira Guedes <liracrg@gmail.com>
2023-08-29 17:34:35 -0300

    feat: Requisito 5

---
+++ challenges/desafio5.sql
@@ -0,0 +1,8 @@
+SELECT a.nome AS cancao,
+  COUNT(b.id_cancoes) AS reproducoes
+FROM SpotifyClone.cancoes AS a
+LEFT JOIN SpotifyClone.historico AS b ON b.id_cancoes = a.id_cancoes
+GROUP BY cancao 
+ORDER BY reproducoes DESC, cancao LIMIT 2;
+
+-- 
